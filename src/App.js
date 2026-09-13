8f7cb45b93263b159385156120064e1563738c7d
ShivamiNautiyal <shivaminautiyal77@gmail.com>
2026-09-13 23:08:09 +0530

    Fix routing for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import ResumeBuilder from "./pages/ResumeBuilder/ResumeBuilder";
 import ResumeAnalyzer from "./pages/ResumeAnalyzer/ResumeAnalyzer";
@@ -11,7 +10,7 @@ import CodeExplainer from "./pages/CodeExplainer/CodeExplainer";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
          <Route path="/resume-builder" element={<ResumeBuilder />} /> 
@@ -21,7 +20,7 @@ function App() {
         <Route path="/interview-prep" element={<InterviewPrep />} />
         <Route path="/code-explainer" element={<CodeExplainer />} /> 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
